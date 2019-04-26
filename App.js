7d90efee5e5d1fa1b508b232ae60e4abcf78cca6
Triobird <lakacey03@gmail.com>
2019-04-26 18:36:39 -0400

    fixed minor error

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { vibrate } from './utils' // Unfortunately, I have a device that doesn't support vibration, so I'm not too sure if vibration works 
